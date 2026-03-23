6acca7e19fdbe7f19e424e56bb5dbfe80e0b7952
Blind-ness-777 <wlsgod10@gmail.com>
2026-03-23 14:15:29 +0900

    feat: [Map] 기획서 반영을 위해 RoomState 상태값 추가

--- Project_LoL/Assets/_Scripts/JJH/RoomNode.cs
+++ Project_LoL/Assets/_Scripts/JJH/RoomNode.cs
@@ -1,5 +1,12 @@
 using System.Collections.Generic;
 
+public enum RoomState
+{
+    Unvisited,   // 미입장
+    InProgress,  // 진행 중
+    Cleared      // 클리어
+}
+
 // 그래프 기반 맵에서 사용하는 노드
 // 각 방(RoomData)을 하나의 노드로 보고 연결 구조를 관리
 public class RoomNode
@@ -8,9 +15,7 @@ public class RoomNode
     
     public RoomData roomData;      // 실제 방 데이터 (타입, 설정 등)
     
-    public bool isVisited;         // 플레이어가 한 번이라도 진입했는지
-    
-    public bool isCleared;         // 해당 방의 이벤트(전투 등) 완료 여부
+    public RoomState state;        // 현재 진행 상태
     
     public List<RoomNode> neighbors; // 연결된 인접 노드 (양방향 그래프)
 
@@ -18,9 +23,8 @@ public class RoomNode
     {
         nodeId = id;
         roomData = data;
-
-        isVisited = false;
-        isCleared = false;
+        
+        state = RoomState.Unvisited;
 
         neighbors = new List<RoomNode>();
     }
