f14016fd74bfb3a2e7cee9868af2ecb420261376
Yuchan Lee <flashedmite45@gmail.com>
2026-03-29 22:18:37 +0900

    feat: 잭팟스트라토스

--- Project_LoL/Assets/_Scripts/LYC/Skill/SkillHitbox.cs
+++ Project_LoL/Assets/_Scripts/LYC/Skill/SkillHitbox.cs
@@ -9,6 +9,11 @@ namespace _Scripts.LYC.Skill
 		private float _duration;
 		private ParticleSystem _effect;
 
+		/// <summary>
+		/// 근접 히트박스를 초기화합니다.
+		/// <param name="direction"> direction이 Vector2.zero이면 방향이 없는 히트박스로 처리됩니다</param>
+		/// <param name="position">히트박스의 실제 생성 위치는 <c>position + direction</c>입니다.</param>
+		/// </summary>
 		public virtual void Init(Vector2 direction, Vector2 position, SkillExecutor executor, float duration,
 			ParticleSystem effect)
 		{
@@ -24,13 +29,16 @@ namespace _Scripts.LYC.Skill
 			if (TryGetComponent(out BoxCollider2D boxCollider))
 			{
 				boxCollider.size = new Vector2(skillData.DamageRangeX, skillData.DamageRangeY);
-				boxCollider.offset = new Vector2(skillData.DamageRangeX / 2.0f, 0);
+				// 방향이 있을 때만 offset 적용: pivot을 끝으로 맞춤
+				boxCollider.offset = new Vector2(direction == Vector2.zero ? 0 : skillData.DamageRangeX / 2.0f, 0);
 			}
 
 			if (effect)
 			{
 				_effect = Instantiate(effect, transform);
-				_effect.transform.SetLocalPositionAndRotation(Vector3.right * skillData.DamageRangeX / 2.0f, Quaternion.identity);
+				// 방향이 있을 때만 이펙트 offset 적용
+				if (direction != Vector2.zero)
+					_effect.transform.SetLocalPositionAndRotation(Vector3.right * skillData.DamageRangeX / 2.0f, Quaternion.identity);
 				_effect.transform.localScale = new Vector3(skillData.DamageRangeX, skillData.DamageRangeY, 1);
 				var main = _effect.main;
 				main.duration = _duration;
