97b30a27a21cbdf69d694b66d1a25ddc5be2c53f
Yuchan Lee <flashedmite45@gmail.com>
2026-03-30 15:58:47 +0900

    feat: 플레이어 Dash 방향에 따라 이펙트 위치 조정

--- Project_LoL/Assets/_Scripts/LYC/Player/PlayerEffectManager.cs
+++ Project_LoL/Assets/_Scripts/LYC/Player/PlayerEffectManager.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 public class PlayerEffectManager : MonoBehaviour
 {
 	[SerializeField] private ParticleSystem _dashParticle;
-	
+
+	private PlayerFSM _fsm;
 	private SpriteRenderer _playerRenderer;
+
 	private float _actionEffectDuration = 0.2f;
 
 	private void Awake()
 	{
 		_playerRenderer = GetComponentInChildren<SpriteRenderer>();
+		_fsm = GetComponent<PlayerFSM>();
 	}
 
 	public void PlayHitEffect()
@@ -21,6 +24,15 @@ public class PlayerEffectManager : MonoBehaviour
 
 	public void PlayDashEffect()
 	{
+		_dashParticle.transform.localPosition = new Vector3(
+			_fsm.FacingDir.x switch
+			{
+				> 0 => -0.35f,
+				< 0 => 0.35f,
+				_ => 0
+			}, 0.35f, 0);
+		_dashParticle.Play();
+
 		_playerRenderer.color = Color.skyBlue;
 		_playerRenderer.DOColor(Color.white, _actionEffectDuration).SetEase(Ease.InCubic);
 
