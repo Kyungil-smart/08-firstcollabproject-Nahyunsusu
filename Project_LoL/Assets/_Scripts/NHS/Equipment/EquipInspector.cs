5284a75cf9488a84d501ab74da602cfb5434d7da
4awol <skgustn3355@gmail.com>
2026-04-03 03:11:54 +0900

    Squashed commit of the following:
    commit cee93348e3bdb9eec345a3830d7cfc207ab917bf
    Merge: 8f147f2 8277b10
    Author: Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
    Date:   Fri Apr 3 01:09:28 2026 +0900
    
        Merge pull request #87 from Kyungil-smart/CJH
    
        feat: 최종보스방 임시 맵 및 전투 시작 연동 문 시스템 구축
    
    commit 8277b10f4c4ea1c8288c715e31231ceb70181bb3
    Author: Blind-ness-777 <wlsgod10@gmail.com>
    Date:   Fri Apr 3 00:04:21 2026 +0900
    
        fix: 보스 처치 연동 상자/포탈 비쥬얼 초기 비활성화 처리
    
    commit efb71d921e17e2ba4f67b33b70e009d609229021
    Author: Blind-ness-777 <wlsgod10@gmail.com>
    Date:   Fri Apr 3 00:00:45 2026 +0900
    
        feat: 최종보스방 임시 맵 및 문 시스템 추가
    
    commit 8f147f24ec7fa2a73ce211e001d95c6037610eba
    Merge: bd0b480 fb3af76
    Author: Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
    Date:   Thu Apr 2 17:46:29 2026 +0900
    
        Merge pull request #86 from Kyungil-smart/NHS_A
    
        [Update]
    
    commit fb3af7617f3b50685f9a98460778486f77effc82
    Author: 4awol <skgustn3355@gmail.com>
    Date:   Thu Apr 2 17:46:06 2026 +0900
    
        [Update]
    Revert "[Update] 강화 ui 업데이트"
    
    This reverts commit 006c4f00f9287d09eed15ca0717f4cda26ceb702.

--- Project_LoL/Assets/_Scripts/NHS/Equipment/EquipInspector.cs
+++ Project_LoL/Assets/_Scripts/NHS/Equipment/EquipInspector.cs
@@ -8,6 +8,22 @@ public class EquipInspector : MonoBehaviour
 
     [SerializeField] private List<Image> images = new List<Image>(4);
 
+    private void OnEnable()
+    {
+        if (_equipmentList != null)
+        {
+            _equipmentList.OnEquipChanged += RefreshUI;
+        }
+        RefreshUI();
+    }
+
+    private void OnDisable()
+    {
+        if (_equipmentList != null)
+        {
+            _equipmentList.OnEquipChanged -= RefreshUI;
+        }
+    }
 
     public void RefreshUI()
     {
